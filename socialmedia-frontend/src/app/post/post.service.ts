c178ac5a3227adecdeeeb730a76eb61347e4a5d8
Leo <a12232856@unet.univie.ac.at>
2025-02-27 17:56:06 +0100

    Add getPostsByUser endpoint, Add page-response model

--- socialmedia-frontend/src/app/post/post.service.ts
+++ socialmedia-frontend/src/app/post/post.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
+import { PageResponse } from '../models/page-response.model';
 
 @Injectable({
     providedIn: 'root'
@@ -17,5 +19,9 @@ export class PostService {
         return this.http.post(`${this.apiUrl}/i`, formData, {
             responseType: 'text'
         });
+    }
+
+    getPostsByUser(username: string, page: number = 0, size: number = 20): Observable<PageResponse<Post>> {
+        return this.http.get<PageResponse<Post>>(`${this.apiUrl}/user/${username}?page=${page}&size=${size}`);
     }
 }
