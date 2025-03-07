ee6f77b434d663d5bce33cd773b6c7bb9d1b7f83
Leo <a12232856@unet.univie.ac.at>
2025-03-07 01:23:51 +0100

    Remove validation for password

--- socialmedia-backend/src/main/java/com/github/leloxo/socialmedia/dto/request/LoginRequest.java
+++ socialmedia-backend/src/main/java/com/github/leloxo/socialmedia/dto/request/LoginRequest.java
@@ -3,7 +3,6 @@ package com.github.leloxo.socialmedia.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,11 +11,12 @@ public class LoginRequest {
     @Email(message = "The email address is invalid.", flags = { Pattern.Flag.CASE_INSENSITIVE })
     private String email;
 
+    // TODO: remove pwd validation here?
     @NotEmpty(message = "Password is required.")
-    @Size(min = 8, message = "Password must be at least 8 characters long.")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$",
-            message = "Password must contain one uppercase, one lowercase, and one number."
-    )
+//    @Size(min = 8, message = "Password must be at least 8 characters long.")
+//    @Pattern(
+//            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$",
+//            message = "Password must contain one uppercase, one lowercase, and one number."
+//    )
     private String password;
 }
